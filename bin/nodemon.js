ebb2fa4ff6f18d4d8a3f62d0978d8b37c4398cf3
Prashant Agarwal <agarwalprashant355@gmail.com>
2023-03-27 11:41:40 +0530

    added comments to nodemon.js file

--- bin/nodemon.js
+++ bin/nodemon.js
@@ -14,3 +14,37 @@ const pkg = JSON.parse(fs.readFileSync(__dirname + '/../package.json'));
 if (pkg.version.indexOf('0.0.0') !== 0 && options.noUpdateNotifier !== true) {
   require('simple-update-notifier')({ pkg });
 }
+
+
+// I'll break down the code and provide an explanation for each line in a way that should be easy for a computer science freshman to understand.
+
+// Line 1: #!/usr/bin/env node
+
+// This is called a "shebang" line, and it tells the operating system to use the Node.js interpreter to execute the rest of the file. This line allows the script to be run as an executable without explicitly invoking the node command.
+// Line 3: const cli = require('../lib/cli');
+
+// This line imports the 'cli' module from the '../lib/cli' file. 'require' is a built-in Node.js function that loads a module (file or package) and assigns its exports to the 'cli' constant.
+// Line 4: const nodemon = require('../lib/');
+
+// This line imports the 'nodemon' module from the '../lib/' directory. The 'require' function looks for an 'index.js' file in the specified directory, which is the main entry point for the 'nodemon' module.
+// Line 5: const options = cli.parse(process.argv);
+
+// This line calls the 'parse' function from the 'cli' module, passing in 'process.argv' as an argument. 'process.argv' is an array containing the command-line arguments passed to the script. The 'parse' function processes the command-line arguments and returns an object containing the parsed options.
+// Line 7: nodemon(options);
+
+// This line calls the 'nodemon' function with the 'options' object, which starts the nodemon process with the specified options.
+// Line 9: const fs = require('fs');
+
+// This line imports the built-in 'fs' (File System) module, which provides an API to interact with the file system.
+// Line 12: const pkg = JSON.parse(fs.readFileSync(__dirname + '/../package.json'));
+
+// This line reads the contents of the 'package.json' file located in the parent directory of the current script. It uses 'fs.readFileSync()' to read the file synchronously (blocking other operations until the file is read). '__dirname' is a built-in variable that represents the directory of the current script file. 'JSON.parse()' then converts the JSON contents of the file into a JavaScript object and assigns it to the 'pkg' constant.
+// Line 14: if (pkg.version.indexOf('0.0.0') !== 0 && options.noUpdateNotifier !== true) {
+
+// This line checks two conditions:
+// If the 'version' property of the 'pkg' object does not start with '0.0.0'.
+// If the 'noUpdateNotifier' property of the 'options' object is not set to 'true'.
+// If both conditions are met, the code inside the following block will be executed.
+// Line 15: require('simple-update-notifier')({ pkg });
+
+// This line imports the 'simple-update-notifier' package and immediately calls the returned function with an object
